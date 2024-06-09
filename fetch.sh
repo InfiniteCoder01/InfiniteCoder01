a6f1d8397748370463ef00743fff15bc1d24856e
InfiniteCoder <info@infinitecoder.org>
2024-06-09 21:52:01 +0300

    Update (Should've done this a long time ago...)

--- fetch.sh
+++
@@ -1,1 +0,0 @@
-cp -r ~/system /mnt/D/Channel/Github
--- system/home-manager/dotfiles/waybar/scripts/powermenu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-window="powermenu-window"
-current_info=$(eww windows | grep "$window")
-
-if [[ $current_info == "*$window" ]]; then
-	eww close $window
-else
-	eww open $window
-fi
